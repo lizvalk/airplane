1286e3f479cd0899a781d03790e665129b6bb9b4
Lisa Valkevich <lizvalk@mail.ru>
2023-03-11 20:49:23 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 50;
+        int oneMileRub = 20;
+
+        int bonus = price / oneMileRub;
+
+        System.out.println("Количество бонусных милей: " + bonus);
+
+    }
+}
